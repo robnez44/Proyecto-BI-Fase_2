5ca08556c4fab3de9221105ecacc9e4394cd192c
robnez44 <00044623@uca.edu.sv>
2025-06-06 01:06:35 -0600

    cambios y proceso ETL

---
+++ Proceso ETL/DataMart.sql
@@ -0,0 +1,53 @@
+CREATE DATABASE Telecom_DataMart;
+
+USE Telecom_DataMart;
+
+-- Dimensiones
+CREATE TABLE DimCliente (
+    id_cliente INT PRIMARY KEY,
+    nombre VARCHAR(100),
+    edad INT,
+    genero VARCHAR(10),
+    correo_electronico VARCHAR(100),
+    fecha_registro DATE
+);
+
+CREATE TABLE DimServicio (
+    id_servicio INT PRIMARY KEY,
+    nombre_servicio VARCHAR(100),
+    tipo VARCHAR(50),
+    costo_mensual DECIMAL(10,2)
+);
+
+CREATE TABLE DimFecha (
+    fecha DATE PRIMARY KEY,
+    anio INT,
+    mes INT,
+    dia INT,
+    nombre_mes VARCHAR(20),
+    semana INT
+);
+
+CREATE TABLE DimRegion (
+    id_region INT PRIMARY KEY,
+    nombre_region VARCHAR(50),
+    zona VARCHAR(20),
+    pais VARCHAR(50)
+);
+
+-- Tabla de hechos
+CREATE TABLE FactConsumo (
+    id_registro INT PRIMARY KEY,
+    id_cliente INT,
+    id_servicio INT,
+    fecha DATE,
+    id_region INT,
+    uso_mb INT,
+    llamadas_minutos INT,
+    fallos_reportados INT,
+    FOREIGN KEY (id_cliente) REFERENCES DimCliente(id_cliente),
+    FOREIGN KEY (id_servicio) REFERENCES DimServicio(id_servicio),
+    FOREIGN KEY (fecha) REFERENCES DimFecha(fecha),
+    FOREIGN KEY (id_region) REFERENCES DimRegion(id_region)
+);
+
